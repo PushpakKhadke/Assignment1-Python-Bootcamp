3370a3ace797213e9684bf1cf99aa80a525b3e87
Pushpak Khadke <100704509+PushpakKhadke@users.noreply.github.com>
2022-08-26 17:47:09 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,10 +1,10 @@
 # 9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
 
-a="Pushpak"
-b=18
-c="Diploma Completed"
-d="Begineer"
-print(a)
-print(b)
-print(c)
-print(d)
+A="Pushpak"
+B=18
+C="Diploma Completed"
+D="Begineer"
+print(A)
+print(B)
+print(C)
+print(D)
