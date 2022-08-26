7ad0ddd83010fc468456bb6714fa53728ee74e35
Pushpak Khadke <100704509+PushpakKhadke@users.noreply.github.com>
2022-08-26 17:44:13 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,11 +1,6 @@
 # 3. Write a python script and store it in a file and execute the file from the command line
 
-from fileinput import filename
-from turtle import screensize
-# 3. Write a python script and store it in a file and execute the file from the command line.3. Write a python script and store it in a file and execute the file from the command line.
-
 """
-
 Step 1: First create a python file any name example (one.py)
 Step 2: Enter the windows+r and open cmd 
 Step 3: Enter file Path example(cd desktop)
