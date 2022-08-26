2dce08d629b225e51ab3d0da3936e78b203edca6
Pushpak Khadke <100704509+PushpakKhadke@users.noreply.github.com>
2022-08-26 17:45:14 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,7 +1,7 @@
 # 5. Write a python script to print Hello on the first line and Python on the second line
 
-# # 1st way
-print('Hello Python',sep="\n")
+# 1st way
+print("Hello Python",sep="\n")
 
 # 2nd way
 print("Hello")
